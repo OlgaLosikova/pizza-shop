7d4ba9a8ac9d652b6403684297eb1a4aa51625f9
losikova.o <losikova.o@modimio.ru>
2026-02-05 12:16:50 +0300

    hashrouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { store } from './store/store';
 import { Provider } from 'react-redux';
 const rootEl = document.getElementById('root');
@@ -10,9 +9,9 @@ if (rootEl) {
   const root = ReactDOM.createRoot(rootEl);
   root.render(
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>,
   );
 }
